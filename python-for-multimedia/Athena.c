6c7bd988a03e9ea21a9ebbaede3acd81f2df6cb3
pogmooney <patrickbrycemooney@gmail.com>
2026-08-16 09:30:16 -0700

    Create Athena.c

---
+++ python-for-multimedia/Athena.c
@@ -0,0 +1,16 @@
+hello 
+
+my name is patrick. 
+
+
+new to programming. contact me please! 
+
+cell phone 650 - 554 - 9485
+
+email : patrickbrycemooney@gmail.com
+
+address : stinson beach , california , usa 
+
+and
+
+san carlos , california , 94070 , usa. 
